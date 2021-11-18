31c2acafcb4bc5d6c484cace5baa0b7feb410c5c
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2021-11-18 15:50:46 +0700

    Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -52,6 +53,13 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(corsPoilicyBuilder => 
+                corsPoilicyBuilder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("https://localhost:4200")
+            );
 
             app.UseAuthorization();
 
